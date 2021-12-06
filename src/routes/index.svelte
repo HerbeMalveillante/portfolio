536368f494b4a5557c46450e88c43c469c9a08d9
Herbe Malveillante <herbemalveillante@gmail.com>
2021-12-06 14:20:40 +0100

    first module

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,17 @@
-<h1>
-    Hello World
-</h1>
+<script>
+
+</script>
+
+
+<main>
+    <div class="flex flex-col justify-center items-center bg-white h-screen w-screen">
+        <div class="flex flex-col items-center">
+            <div class="relative group">
+                <h1 class="absolute lg:text-9xl md:text-7xl text-5xl font-ubuntu font-black bg-gradient-to-r from-pink-500 to-blue-500 bg-clip-text text-transparent filter blur-xl animate-pulse duration-500">Herbe Malveillante</h1>
+                <h1 class="relative lg:text-9xl md:text-7xl text-5xl font-ubuntu font-black bg-gradient-to-r from-pink-500 to-blue-500 bg-clip-text text-transparent">Herbe Malveillante</h1>
+            </div>
+            <h2 class="lg:text-4xl md:text-2xl text-xl font-ubuntu">Fullstack Web Developer</h2>
+            <svg class="lg:w-20 lg:h-20 md:w-12 md:h-12 w-8 h-8 text-blue-500  lg:mt-5 sm:mt-3 hover:text-pink-500 hover:rotate-[360deg] transition duration-500 ease-in-out hover:scale-125" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z" clip-rule="evenodd"></path></svg>
+        </div>
+    </div>
+</main>
